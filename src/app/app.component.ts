237e8d1f1edd50e0f9ae124673797975b11b4836
Vlad Retca <vlad.retca@gmail.com>
2022-01-31 16:18:39 +0200

    add tracking link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
-
+import {Component, OnInit} from '@angular/core';
+import Tracker from '@openreplay/tracker';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   constructor() {}
+
+  ngOnInit(): void {
+    const tracker = new Tracker({
+      projectKey: 'aEQbM7n8xayv1WkVDLpx',
+      __DISABLE_SECURE_MODE:true
+    });
+    tracker.start();
+    console.log("tracker started")
+  }
 }
